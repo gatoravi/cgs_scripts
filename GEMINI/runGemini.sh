65f1ffcfec46bcd5e2b53b1a4716a70d8215fc33
Avinash Ramu <trackavinash@gmail.com>
2016-04-11 18:56:55 -0500

    Update GEMINI script

--- GEMINI/runGemini.sh
+++ GEMINI/runGemini.sh
@@ -1,29 +1,28 @@
 #VEP is on clia1 here - /opt/gms/vep/variant_effect_predictor
 
-INPUT_VCF=snvs.vcf.gz
-VCFTOOLS_PREFIX=snvs_filteredDP10
-VCFTOOLS_VCF=snvs_filteredDP10.recode.vcf
-VEP_OUTPUT=snvs_annotated.vcf
-VEP_OUTPUT_GZ=snvs_annotated.vcf.gz
+set -e
+INPUT_VCF=snvs_indels.vcf.gz
+VCFTOOLS_PREFIX=snvs_indels_filteredDP10
+VCFTOOLS_VCF=snvs_indels_filteredDP10.recode.vcf
+VEP_OUTPUT=snvs_indels_annotated.vcf
+VEP_OUTPUT_GZ=snvs_indels_annotated.vcf.gz
 VEP_STATS=vep_stats.html
-GEMINI_DB=GTB15.db
-
-#Get the variants from the ref-align models
-#scp aramu@linus217:/gscmnt/gc13022/info/model_data/9ade7950b05a4ec7a77f6a7fb9ab207e/build9443487effb247c1abc11335d5dbbb05/variants/snvs.vcf.gz .
+GEMINI_DB=GTB02.db
 
 #Filter input VCF based on depth
-#vcftools --min-meanDP 10 --gzvcf ${INPUT_VCF} --out ${VCFTOOLS_PREFIX} --recode
+vcftools --min-meanDP 10 --gzvcf ${INPUT_VCF} --out ${VCFTOOLS_PREFIX} --recode
 
 #another example with required VEP annotations for GEMINI
-#rm -f ${VEP_OUTPUT} ${VEP_STATS}
-#PERL5LIB=$PERL5LIB:/opt/gms/vep/ perl /opt/gms/vep/variant_effect_predictor/variant_effect_predictor.pl -i ${VCFTOOLS_VCF} --cache --sift b --polyphen b --symbol --numbers --biotype --gmaf --maf_1kg --maf_esp --maf_exac --total_length -o ${VEP_OUTPUT} --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ExAC_MAF,GMAF --dir_cache /opt/gms/vep/v83/ --assembly GRCh37 --offline --stats_file ${VEP_STATS} --fork 20
+rm -f ${VEP_OUTPUT} ${VEP_STATS}
+PERL5LIB=$PERL5LIB:/opt/gms/vep/ perl /opt/gms/vep/variant_effect_predictor/variant_effect_predictor.pl -i ${VCFTOOLS_VCF} --cache --sift b --polyphen b --symbol --numbers --biotype --gmaf --maf_1kg --maf_esp --maf_exac --total_length -o ${VEP_OUTPUT} --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ExAC_MAF,GMAF --dir_cache /opt/gms/vep/v83/ --assembly GRCh37 --offline --stats_file ${VEP_STATS} --fork 20
 
 #Index the VCF file with tabix
-#tabix -f -p vcf ${VEP_OUTPUT_GZ}
+bgzip ${VEP_OUTPUT}
+tabix -f -p vcf ${VEP_OUTPUT_GZ}
 
 #load the VCF into Gemini
 #Gemini is installed here - /opt/gms/gemini/bin/gemini
-#PATH=$PATH:/opt/gms/gemini/bin gemini load -v ${VEP_OUTPUT_GZ} -t VEP ${GEMINI_DB} --cores 30
+PATH=$PATH:/opt/gms/gemini/bin gemini load -v ${VEP_OUTPUT_GZ} -t VEP ${GEMINI_DB} --cores 30
 
 #query with clinvar
 /opt/gms/gemini/bin/gemini query --header -q "SELECT v.chrom,v.end,v.qual,v.grc,v.gms_illumina,v.in_segdup,v.filter,v.num_het,v.aaf,v.max_aaf_all,v.gene,v.cosmic_ids,v.biotype,v.impact,v.impact_severity,v.aa_change,v.rs_ids,v.sift_pred,v.polyphen_pred,v.cadd_scaled,v.in_omim,v.clinvar_disease_name FROM variants v, gene_summary g WHERE v.gene=g.gene AND  v.qual>='10' AND v.max_aaf_all<='0.01' AND v.is_coding==1 AND v.impact!='synonymous_variant' AND v.impact!='intron' AND v.impact!='downstream' AND v.impact!='upstream' AND v.impact_severity!='LOW' AND v.in_segdup=0 AND v.in_cse=0 AND v.sift_pred!='tolerated' AND v.polyphen_pred!='benign' AND g.in_cosmic_census==1" ${GEMINI_DB}
